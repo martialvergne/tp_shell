00bf336f45d9cb79aeb041106e4037fecdbfb8bd
Martial Vergne <105485322+martialvergne@users.noreply.github.com>
2023-06-30 11:14:03 +0200

    Update verif-pwd.sh

--- verif-pwd.sh
+++ verif-pwd.sh
@@ -12,8 +12,8 @@
 #Vérification si le mot de passe est passé en paramètre au lancement du script
 if [ $# -eq 0 ]
 then
-	read -p "Entrez le mot de passe à tester : " password
-	echo "Vérification du mot de passe saisi"
+	read -sp "Entrez le mot de passe à tester : " password
+	echo "\n Vérification du mot de passe saisi"
 else 
 	password=$1
 fi
@@ -23,9 +23,18 @@ fi
 test=$(expr length $password)
 if [ "$test" -lt 16 ]
 then
-	echo "Erreur. Le mot de passe saisi contient moins de 16 caractères"
-	pwd_suggest=$(openssl rand -base64 16)
-	echo "Mot de passe suggéré : $pwd_suggest"
+	echo "\n Erreur. Le mot de passe saisi contient moins de 16 caractères"
+	pwd_suggest=$(openssl rand -base64 12)
+	echo "\n Mot de passe suggéré : $pwd_suggest"
 else
-	echo "Ok. Le mot de passe saisi contient 16 caractères ou plus"
+	echo "\n Ok. Le mot de passe saisi contient 16 caractères ou plus"
 fi
+if [[ $password =~ [A-Z]] && [[ $password =~ [a-z]] && [[ $password =~ [0-9]] && [[ $password =~ [@\#\$\%\!\?\&\+\=\;] ]]
+then
+	echo "\n La complexité du mot de passe est suffisante"
+else
+	echo "\n La complexité du mot de passe n'est pas suffisante"
+	pwd_suggest=$(openssl rand -base64 12)
+	echo "\n Mot de passe suggéré : $pwd_suggest"
+fi
+
