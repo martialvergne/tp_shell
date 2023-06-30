c0ef7931c60e0ad46931feffa1531cb685c9820d
Martial Vergne <105485322+martialvergne@users.noreply.github.com>
2023-06-30 10:04:26 +0200

    Update verif-pwd.sh

--- verif-pwd.sh
+++ verif-pwd.sh
@@ -10,11 +10,20 @@
 ##################
 
 
-if [ $# eq 0 ]
+if [ $# -eq 0 ]
 then
 	read -p "Entrez le mot de passe à tester : " passwd
-	echo "Vérification du mot de passe saisi $passwd"
+	echo "Vérification du mot de passe saisi"
 else 
-	passwd = $1
-	echo "password = $passwd"
+	passwd=$1
+fi
+
+test=$(expr length $passwd)
+if [ "$test" -lt 16 ]
+then
+	echo "Erreur. Le mot de passe saisi contient moins de 16 caractères"
+	pwd_suggest=$(openssl rand base64 16)
+	echo "Mot de passe suggéré : $pwd_suggest"
+else
+	echo "Ok. Le mot de passe saisi contient 16 caractères ou plus"
 fi
