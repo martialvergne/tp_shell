8fd5130e8e9f843e073c4c7ce84d2a58682dbb94
Martial Vergne <105485322+martialvergne@users.noreply.github.com>
2023-06-30 08:57:30 +0200

    Update verif-CIS.sh

--- verif-CIS.sh
+++ verif-CIS.sh
@@ -10,14 +10,14 @@
 ##################
 
 echo "Vérification des recommandations CIS"
-findmnt --kernel /tmp
+findmnt --kernel /tmp > /dev/null 2>&1
 if [ $? -ne 0 ]
 then
-	systemctl- is-enabled tmp.mount
+	systemctl- is-enabled tmp.mount > /dev/null 2>&1
 	if [ $? -ne 0 ]
 	then
 		echo "La configuration est non conforme, modification du fstab"
-		systemctl unmask tmp.mount
+		systemctl unmask tmp.mount > /dev/null 2>&1
 		echo "tmpfs	/tmp	tmpfs	defaults,rw,nosuid,nodev,noexec,relatime,size=2G	0	0" >> /etc/fstab
 		echo "[X] 1.1.2.1 - Le montage /tmp est sur un montage séparée"
 		echo "[X] 1.1.2.2 - Nodev option set on /tmp"
@@ -25,32 +25,34 @@ then
 		echo "[X] 1.1.2.4 - Nosuid option set on /tmp"
 		exit 0
 	fi
-	findmnt --kernel /tmp
+	findmnt --kernel /tmp > /dev/null 2>&1
+else
+	echo echo "[X] 1.1.2.1 - Le montage /tmp est sur un montage séparée" 
 fi
 
-findmnt --kernel /tmp | grep nodev
+findmnt --kernel /tmp | grep nodev > /dev/null 2>&1
 if [ $? -ne 0 ]
 then
-	echo "[ ] 1.1.2.2 - Nodev option not set on /tmp" >> output.txt
+	echo "[ ] 1.1.2.2 - Nodev option not set on /tmp" 
 else
-	echo "[X] 1.1.2.2 - Nodev option set on /tmp" >> output.txt
+	echo "[X] 1.1.2.2 - Nodev option set on /tmp" 
+fi
+mount -o remount /tmp 
+
+findmnt --kernel /tmp | grep noexec > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+	echo "[ ] 1.1.2.3 - Noexec option not set on /tmp"
+else
+	echo "[X] 1.1.2.3 - Noexec option set on /tmp"
 fi
 mount -o remount /tmp
 
-findmnt --kernel /tmp | grep noexec
+findmnt --kernel /tmp | grep nosuid > /dev/null 2>&1
 if [ $? -ne 0 ]
 then
-	echo "[ ] 1.1.2.3 - Noexec option not set on /tmp" >> output.txt
+	echo "[ ] 1.1.2.4 - Nosuid option not set on /tmp"
 else
-	echo "[X] 1.1.2.3 - Noexec option set on /tmp" >> output.txt
-fi
-mount -o remount /tmp
-
-findmnt --kernel /tmp | grep nosuid
-if [ $? -ne 0 ]
-then
-	echo "[ ] 1.1.2.4 - Nosuid option not set on /tmp" >> output.txt
-else
-	echo "[X] 1.1.2.4 - Nosuid option set on /tmp" >> output.txt
+	echo "[X] 1.1.2.4 - Nosuid option set on /tmp"
 fi
 mount -o remount /tmp
